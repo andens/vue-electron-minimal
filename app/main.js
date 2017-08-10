a2c92a8aed967444e8adf3377e85cff5ad4b4507
Andreas Flöjt <andenzz@hotmail.com>
2017-08-10 13:32:59 +0200

    Updated info comments

--- app/main.js
+++ app/main.js
@@ -14,7 +14,14 @@ new Vue({
 // Projekt som använder Vue https://github.com/vuejs/awesome-vue#projects-using-vuejs
 // Electron-app som använder Vue, kan se hur de har satt ihop det hela https://github.com/Zhangdroid/Gokotta
 // Guide https://vuejs.org/v2/guide/
-// Kan vara värd att läsa om när jag fått en rotkomponent att fungera och där index använder dist/build.js utan att behöva pilla där manuellt: https://scotch.io/tutorials/create-a-desktop-quiz-application-using-vue-js-and-electron
-// https://scotch.io/tutorials/build-a-single-page-time-tracking-app-with-vue-js-introduction
-// https://scotch.io/tutorials/how-to-build-a-simple-single-page-application-using-vue-2-part-1
+// https://devblog.digimondo.io/building-a-json-tree-view-component-in-vue-js-from-scratch-in-six-steps-ce0c05c2fdd8
+// https://scotch.io/tutorials/how-to-handle-file-uploads-in-vue-2
+// Bra video som visar många intressanta aspekter av Vue https://www.youtube.com/watch?v=p-7Zi9xYt2M
+// Serie av videor https://laracasts.com/series/learn-vue-2-step-by-step/episodes/1
+
+
+// För dropdown: custom directive på items som informerar sin parent så länge den är av rätt
+// typ när de klickas eller tas bort eller liknande. Då behöver inte parent bry sig om att
+// hitta sina children.
+// Kanske användbar: https://forum.vuejs.org/t/how-to-properly-access-components-inside-slot-content/4713/6
 
